53f7af951124668a2bd2de9ebf49123dea188e77
Daniel Jung <endjung77@gmail.com>
2022-10-16 00:31:58 +0900

    test

--- test.R
+++ test.R
@@ -42,4 +42,32 @@ ggplot(data = plot_data, aes(x = x, y = y)) +
            label = TeX("$f(x) = \\sum_{i=1}^{n}(x_i - \\mu)^2$"))
 
 library(scatterplot3d)
-library(magritter)
+library(magrittr)
+
+x1 <- x2 <- seq(-1, 1, by = 0.1)
+
+myf <- function(vec){
+  0.3 * (vec[1]^2 + vec[2]^2)
+}
+
+values <- expand.grid(x1, x2) %>% 
+  apply(1, myf) %>% 
+  matrix(ncol = length(x1))
+
+s3d <- scatterplot3d(x1, x2,
+    seq(min(values), max(values), length = length(x1)),
+    axis = TRUE, type = "n", grid = FALSE, angle = 70,
+    zlab = expression(f(x[1], x[2])),
+    xlab = expression(x[1]), ylab = expression(x[2]))
+
+#install.packages("mosaic")
+#install.packages("manipulate")
+library(mosaic)
+library(manipulate)
+plotFun(0.3 * (x1^2 + x2^2) ~ x1 & x2,
+        x1.lim = range(-3, 3),
+        x2.lim = range(-3, 3),
+        surface = TRUE,
+        xlab = expression(x[1]),
+        ylab = expression(x[2]),
+        zlab = expression(f(x[1], x[2])), rot = 10)
